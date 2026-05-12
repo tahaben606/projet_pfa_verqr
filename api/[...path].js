8ada7cb1b5339e9bce84442507694111b9773aa0
tahaben606 <taha2ben2@gmail.com>
2026-05-12 11:36:01 +0100

    Failed to fetch

---
+++ api/[...path].js
@@ -0,0 +1,5 @@
+import { createApp } from '../server/src/app.js';
+
+const app = createApp();
+
+export default app;
--- server/src/vercel.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from './app.js';
-
-const app = createApp();
-
-export default app;
